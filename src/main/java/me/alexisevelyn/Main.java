68d7b31cb7eade749281419198bd88bf2b91f05b
Alex Evelyn <alexis.a.evelyn@gmail.com>
2020-09-25 14:37:35 -0400

    Saving Current Links

---
+++ src/main/java/me/alexisevelyn/Main.java
@@ -0,0 +1,12 @@
+package me.alexisevelyn;
+
+// It appears pre-formatted blocks are the only ones that don't follow the new tag after empty line rule.
+// Pull Example Document From: https://github.com/edx/edx-documentation/blob/master/en_us/edx_style_guide/source/ExampleRSTFile.rst
+// reST format: https://docutils.readthedocs.io/en/sphinx-docs/user/rst/quickstart.html
+// Test Editor: https://livesphinx.herokuapp.com/
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Test Load!!!");
+	}
+}
